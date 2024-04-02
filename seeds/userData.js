99c819c3d8eb0fdb75981abd5bbbfc4c7bf2768e
Eric Lee <eric.hyunil.lee@gmail.com>
2024-04-02 00:23:19 -0700

    Updated session secret that wasn't matching on render

--- seeds/userData.js
+++ seeds/userData.js
@@ -5,19 +5,16 @@ const userData = [
     username: "ericeya",
     email: "eric@gmail.com",
     password: "password",
-    blogpost_id: [1]
   },
   {
     username: "daniellee",
     email: "daniel@gmail.com",
     password: "password",
-    blogpost_id: [2]
   },
   {
     username: "joethecat",
     email: "joe@gmail.com",
     password: "password",
-    blogpost_id: [3]
   },
  
   
